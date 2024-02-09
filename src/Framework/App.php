6b59f9768316c8f80eb4c2d589d234ed001a4993
Vitalii <flatronsamatron@gmail.com>
2024-02-09 16:30:15 +0200

    add routing

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -5,7 +5,23 @@ declare(strict_types=1);
 namespace Framework;
 
 class App {
-    public function run(){
-        echo 'app';
+    private Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router(); //object(Framework\App)#4 (1) { ["routers":"Framework\App":private]=> object(Framework\Router)#2 (0) { } } app
+    }
+
+    public function run()
+    {
+        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $this->router->dispatch($path, $method);
+    }
+
+    public function get(string $path, array $controller)
+    {
+        $this->router->add('GET', $path, $controller);
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+include __DIR__ . '/../App/functions.php';
 $app = include __DIR__ . '/../App/bootstrap.php';
 
 $app->run();
